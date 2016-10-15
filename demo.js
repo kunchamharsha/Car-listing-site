a52bcf36887c164131c9b3c16d475aed2f50db2e
ram <kunchamharsha@gmail.com>
2016-10-15 22:19:58 +0530

    complete backend integration ui tweaking left

--- demo.js
+++ demo.js
@@ -1,9 +1,10 @@
 var app=angular.module("spegray",[]);
 app.controller("search",function($http,$scope){
 	$scope.search=function(){
-		var a=$scope.query
-		$http.get("http://localhost:8983/solr/cars/select?indent=on&q=maruti&wt=json").then(function(response){
-			console.log(response.data.response)
+		var url="http://localhost:8983/solr/cars/select?indent=on&q="+$scope.query+"&wt=json"
+		$http.get(url).then(function(response){
+			console.log(response.data.response.docs)
+			$scope.result=response.data.response.docs
 		})
 
 	}
