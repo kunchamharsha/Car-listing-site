385bd899cd987cdae7b3653a22bfdca64f45ceec
ram <kunchamharsha@gmail.com>
2016-10-15 22:48:32 +0530

    tweaked angular ui

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@ var app=angular.module("spegray",[]);
 app.controller("search",function($http,$scope){
 	$scope.search=function(){
 		var url="http://localhost:8983/solr/cars/select?indent=on&q="+$scope.query+"&wt=json"
-		$http.get(url).then(function(response){
+		$http.get(url,{cache:true}).then(function(response){
 			console.log(response.data.response.docs)
 			$scope.result=response.data.response.docs
 		})
