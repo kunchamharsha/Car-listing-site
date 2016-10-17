7f24b0cbaf36fa20213664b94f2b62289556c848
ram <kunchamharsha@gmail.com>
2016-10-17 10:01:42 +0530

    completed search ui

--- demo.js
+++ demo.js
@@ -1,11 +1,23 @@
 var app=angular.module("spegray",[]);
-app.controller("search",function($http,$scope){
+app.config(function($locationProvider) {
+  $locationProvider.html5Mode({
+  enabled:false,
+  requireBase: false
+});
+});
+app.controller("search",function($http,$scope,$location){
 	$scope.search=function(){
 		var url="http://localhost:8983/solr/cars/select?indent=on&q="+$scope.query+"&wt=json"
 		$http.get(url,{cache:true}).then(function(response){
 			console.log(response.data.response.docs)
 			$scope.result=response.data.response.docs
+			$location.search('cars',$scope.query)
 		})
 
+	}
+	$scope.check=function(){
+		var data=$location.search()
+		$scope.query=data.cars
+		$scope.search()		
 	}
 });
